2b8ac57fc9d0dc263aaea70f6e1d6bd16bce59cb
Yanina-Unrein <unrein.syanina@gmail.com>
2025-10-27 18:37:51 -0300

    archivos enviroments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  tmdbApiKey: '', 
+  tmdbApiUrl: 'https://api.themoviedb.org/3'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  tmdbApiKey: 'TU_API_KEY_LOCAL', 
+  tmdbApiUrl: 'https://api.themoviedb.org/3'
+};
